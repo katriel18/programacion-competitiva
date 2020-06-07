3b655268a011a7c56ecf76e37f259948dc0e2555
katriel <katriel181920@gmail.com>
2020-06-07 12:47:31 -0500

    p11498-v1

---
+++ p11498.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=1,x,y, a, b;  
+    do{
+	    cin >> i;
+	    if (i==0) break;
+		cin >>x>>y;				
+	    while(i>0){			
+	        cin >> a >> b ;  
+			a=a-x;
+			b=b-y;	
+			if (a ==0 || b==0)
+			   cout<<"DIVISA"<<endl;
+	        else if (a >0 && b>0)
+	        	cout<<"NE"<<endl;
+	    	else if (a > 0 && b<0) 
+	        	cout<<"SE "<<endl;
+	        else if (a < 0 && b>0) 
+	        	cout<<"NO "<<endl;
+	        else
+	        	cout<<"SO "<<endl;  
+	        i--;
+   		}
+    
+    }while(true);
+    
+    return 0;
+}
